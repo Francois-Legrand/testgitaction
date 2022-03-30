af6b8e5e125f2e942b83f053095d83afcc32bc0d
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2022-03-30 10:32:44 +0200

    first commit

---
+++ src/main/java/fr/semifir/Main.java
@@ -0,0 +1,9 @@
+package fr.semifir;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //methods main
+    }
+
+}
---
+++ src/test/java/fr/semifir/CalculatriceTest.java
@@ -0,0 +1,28 @@
+package fr.semifir;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatriceTest {
+
+    @Test
+    public void addition() {
+        int result = Calculatrice.addition(10,10,10);
+        assertEquals(30, result);
+    }
+    @Test
+    public void soustraction() {
+        int result = Calculatrice.soustraction(10,2,3);
+        assertEquals(5, result);
+    }
+    @Test
+    public void multiplication() {
+        int result = Calculatrice.multiplication(10,10);
+        assertEquals(100, result);
+    }
+    @Test
+    public void division() {
+        int result = Calculatrice.division(10,2);
+        assertEquals(5, result);
+    }
+}
